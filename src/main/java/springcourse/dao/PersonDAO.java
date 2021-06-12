2a13f44b05c886dd99911821b7e7549f6441720c
Ivan_Babushkin <babushkinnavi.ib@gmail.com>
2021-06-12 23:32:16 +0300

    commit

--- src/main/java/springcourse/dao/PersonDAO.java
+++ src/main/java/springcourse/dao/PersonDAO.java
@@ -12,9 +12,12 @@ import java.util.List;
  */
 @Component
 public class PersonDAO {
-    private static final String URL = "jdbc:mysql://192.168.0.91:3306/first_db";
+    private static final String URL = "jdbc:mysql://localhost/first_db";
+//    private static final String USERNAME = "root";
+//    private static final String PASSWORD = "RI43Jkf038fve";
+
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "RI43Jkf038fve";
+    private static final String PASSWORD = "";
 
     private static Connection connection;
 
